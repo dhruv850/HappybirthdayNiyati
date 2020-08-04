64c90ac8aaec95b04d72783ecb867a104a6d2e12
Dhruv Patel <dhruv850.dp@gmail.com>
2020-08-03 21:02:52 -0400

    test done

--- effect.js
+++ effect.js
@@ -169,11 +169,11 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(1600).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('fast').delay(8).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(2000);
+			$("p:nth-child("+i+")").fadeIn('fast').delay(10);
 			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+				$("p:nth-child(49)").fadeOut('fast').promise().done(function () {
 					$('.cake').fadeIn('fast');
 					$('#surprise').fadeIn('slow');
 				});
